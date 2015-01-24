784c4822c4bbdbc6cad6af25e9dfac26661b1b73
aries sanchez sulit <aries.sulit@gmail.com>
2015-01-25 00:37:29 +0800

    Fixed the "tap scale" bug of hazards.

--- BoatTapper/Assets/Scripts/CollisionBox.cs
+++ BoatTapper/Assets/Scripts/CollisionBox.cs
@@ -22,6 +22,8 @@ public class CollisionBox : MonoBehaviour
 		GameObject go = (GameObject)GameObject.Instantiate(m_hazardTemplate.gameObject, p_position, Quaternion.identity);
 		Hazard hazard = go.GetComponent<Hazard>();
 
+		this.Log("CollisionBox::CreatHazard", "ScaleX:{0} Y:{1}", go.transform.localScale.x, go.transform.localScale.y);
+
 		// randomize positions
 		if (GameLogic.Instance != null && hazard.TapType == this.TapType)
 		{
