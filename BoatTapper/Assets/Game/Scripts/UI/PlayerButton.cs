cec1621f32e73e92679704715c7fed5b5a042252
Xander Cruz <xander.m.cruz@gmail.com>
2015-01-24 16:55:12 +0800

    Added UI Triggers

---
+++ BoatTapper/Assets/Game/Scripts/UI/PlayerButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Player
+{
+	Player1,
+	Player2,
+};
+
+public class PlayerButton : MonoBehaviour 
+{
+	[SerializeField]
+	private Player m_player;
+	[SerializeField]
+	private TapType m_type;
+	[SerializeField]
+	private bool m_isEnabled;
+
+	public Signal OnTriggerAction = new Signal(typeof(PlayerButton));
+	
+	private void OnClicked ()
+	{
+		this.OnTriggerAction.Invoke(this);
+	}
+
+	public Player Player 
+	{
+		get { return m_player; }
+		private set { m_player = value; }
+	}
+
+	public TapType TapType 
+	{
+		get { return m_type; }
+		private set { m_type = value; }
+	}
+
+	public bool IsEnabled 
+	{
+		get { return m_isEnabled; }
+		private set { m_isEnabled = value; }
+	}
+}
---
+++ BoatTapper/Assets/Game/Scripts/UI/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Extensions;
+
+public class UIManager : MonoBehaviour 
+{
+	[SerializeField] private List<PlayerButton> m_buttons;
+
+	private void Start ()
+	{
+	}
+
+	[Signal]
+	private void OnPressedButton (PlayerButton p_button)
+	{
+		this.Log("UIManager::OnPressedButton", "Player:{0} Action:{1}", p_button.Player, p_button.TapType);
+	}
+}
