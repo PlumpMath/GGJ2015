fc2115ca77247d015155a42595e6eac1c9ceb6e7
Xander Cruz <xander.m.cruz@gmail.com>
2015-01-24 18:02:47 +0800

    Added cannonball spawner.

---
+++ BoatTapper/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Extensions;
+
+public class BallSpawner : MonoBehaviour 
+{
+	public readonly float EASY_SPAWN = 10f;
+	public readonly float MEDIUM_SPAWN = 6f;
+	public readonly float HARD_SPAWN = 4f;
+
+	[SerializeField]
+	private CannonBall m_ballTemplate;
+	[SerializeField]
+	private Vector2 m_throwForce;
+	private List<CannonBall> m_balls;
+	private float m_interval;
+
+	private void Start ()
+	{
+		this.Assert<CannonBall>(m_ballTemplate, "ERROR: m_ballTemplate must not be null.");
+
+		// initializations
+		m_balls = new List<CannonBall>();
+		m_throwForce = new Vector2(-1024f, 768f) * 0.15f;
+	}
+
+	private void Update ()
+	{
+		m_interval += Time.deltaTime;
+
+		if (m_interval >= HARD_SPAWN) 
+		{
+			m_interval = 0f;
+			this.Throw(3);
+		}
+	}
+
+	public void Throw (int p_num)
+	{
+		GameObject obj = (GameObject)GameObject.Instantiate(m_ballTemplate.gameObject);
+		CannonBall ball = obj.GetComponent<CannonBall>();
+		ball.OnDestroyBall += this.OnDestroyBall;
+		ball.Throw(m_throwForce);
+		ball.transform.parent = this.transform;
+		ball.transform.position = this.transform.position;
+
+		m_balls.Add(ball);
+	}
+
+	private void OnDestroyBall (CannonBall p_ball)
+	{
+		m_balls.Remove(p_ball);
+		GameObject.Destroy(p_ball.gameObject);
+	}
+}
---
+++ BoatTapper/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CannonBall : MonoBehaviour 
+{
+	public event Action<CannonBall> OnDestroyBall;
+	
+	public void Throw (Vector2 p_throwForce)
+	{
+		this.rigidbody2D.AddForce(p_throwForce);
+	}
+
+	private void DestroyBall ()
+	{
+		if (this.OnDestroyBall == null) { return; }
+		this.OnDestroyBall(this);
+	}
+}
