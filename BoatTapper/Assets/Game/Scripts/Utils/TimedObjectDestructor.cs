80b9c4879be42f17b24f2f194a64e731913b8d7b
aries sanchez sulit <aries.sulit@gmail.com>
2015-01-25 11:26:43 +0800

    Explosion and FIYAAAA!

---
+++ BoatTapper/Assets/Game/Scripts/Utils/TimedObjectDestructor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedObjectDestructor : MonoBehaviour {
+
+	public float m_delay = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DestroyAfterSeconds());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator DestroyAfterSeconds()
+	{
+		yield return new WaitForSeconds(m_delay);
+		Destroy(this.gameObject);
+	}
+}
